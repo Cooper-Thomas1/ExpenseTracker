d326f5d3e3beb4e3fbed41fa486e7554db23768b
Cooper Thomas <142972035+Cooper-Thomas1@users.noreply.github.com>
2025-04-16 12:25:31 +0800

    Added routes for html files

--- ExpenseTracker/routes.py
+++ ExpenseTracker/routes.py
@@ -2,5 +2,14 @@ from ExpenseTracker import website
 from flask import render_template
 
 @website.route("/")
-def index():
+@website.route("/home")
+def home():
     return render_template("home.html")
+
+@website.route("/login")
+def login():
+    return render_template("login.html")
+
+@website.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
