177db03a0f8d16bdab9448256cea37b83a4c9f72
Cooper Thomas <142972035+Cooper-Thomas1@users.noreply.github.com>
2025-04-17 13:53:52 +0800

    Resolved conflict between init and routes file

--- ExpenseTracker/__init__.py
+++ ExpenseTracker/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
 # Initialize Flask app
-website = Flask(__name__)
+app = Flask(__name__)
 
 from ExpenseTracker import routes
 
 # Run the app
 if __name__ == "__main__":
-    website.run(debug=True)
+    app.run(debug=True)
