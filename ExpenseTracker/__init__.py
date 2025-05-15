a97b927490a3e541b2e5a71e51c4c59c929e5944
LJW-Dev <lukewaters757@gmail.com>
2025-05-15 22:27:27 +0800

    Added functionality to switch between testing and production databases

--- ExpenseTracker/__init__.py
+++ ExpenseTracker/__init__.py
@@ -12,8 +12,16 @@ load_dotenv()
 
 # Initialize Flask app
 app = Flask(__name__)
+
+# use a database in memory if we're in a testing environment
+testing_environment = os.getenv('SELENIUM_TESTING') == 'True'
+if testing_environment:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+    print("Selenium testing database is in use.")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+    
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 csrf = CSRFProtect(app)
 
 app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
@@ -30,6 +38,12 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+
+# create an empty database if we're in a testing environment
+if testing_environment:
+    from ExpenseTracker import models
+    with app.app_context():
+        db.create_all()
 
 from ExpenseTracker import routes
 
